d65cebf03d021123c6baeaa6e974ce094e73924c
Lavendor <mikolaj.jurecki@gmail.com>
2024-01-04 22:52:05 +0100

    Add OpenApi Configuration

--- src/main/java/com/lavendor/mealmate/controller/RecipeController.java
+++ src/main/java/com/lavendor/mealmate/controller/RecipeController.java
@@ -4,16 +4,12 @@ import com.lavendor.mealmate.model.Recipe;
 import com.lavendor.mealmate.service.RecipeService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/recipe")
 public class RecipeController {
 
--- src/main/java/com/lavendor/mealmate/controller/RecipeIngredientController.java
+++ src/main/java/com/lavendor/mealmate/controller/RecipeIngredientController.java
@@ -4,15 +4,15 @@ import com.lavendor.mealmate.model.RecipeIngredient;
 import com.lavendor.mealmate.service.RecipeIngredientService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collections;
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/recipe-ingredient")
 public class RecipeIngredientController {
 
